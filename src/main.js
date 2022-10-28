1aa3670aa1e30fa8155f7bb27a4c78371993d2ff
llleeeaaannn <piercefrank10@gmail.com>
2022-10-28 03:49:48 +0100

    Refactored code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './style.css';
+import { slogans } from './variables'
+import FallingMan from './falling-man.jpg';
+import PeoplePower from './people-power.jpg';
 
 export default class Main {
 
@@ -7,11 +10,33 @@ export default class Main {
   }
 
   start() {
+    this.createUI();
     this.addListeners();
+  }
+
+  createUI() {
+    this.randomSlogan();
+    this.loadImages();
   }
 
   addListeners() {
     this.menuToggleListener();
+  }
+
+  randomSlogan() {
+    const sloganContainer = document.getElementById('slogan-container');
+    const index = Math.floor(Math.random() * slogans.length);
+    const slogan = slogans[index];
+    sloganContainer.textContent = slogan;
+  }
+
+  loadImages() {
+    const imageContainer = document.getElementById('image-container');
+    const fallingManImage = new Image();
+    const peoplePowerImage = new Image();
+    fallingManImage.src = FallingMan;
+    peoplePowerImage.src = PeoplePower;
+    imageContainer.append(fallingManImage, peoplePowerImage);
   }
 
   menuToggleListener() {
---
+++ src/variables.js
@@ -0,0 +1,3 @@
+export { slogans }
+
+const slogans = ["Your government doesn't care about you", "Your politicians do not care about you", "Your government has nothing but contempt for you", "The duty of a patriot is to protect his country from its government", "Love your country, not your government", "If voting changed anything, they'd make it illegal"]
